6d89b56b8f7be1b37a43bc8bd531e16a0e924e99
AyoungYa <xiaoyang891112@gmail.com>
2014-10-20 20:25:27 +0800

    Revert "Leetcode_in_python"
    This reverts commit abcde7183fe57aa2ef7f01ef504bfa8614bdee73.

--- leetcode/Count_and_Say.py
+++ leetcode/Count_and_Say.py
@@ -1,3 +1,4 @@
+#! -*- coding: utf-8 -*-
 """
 The count-and-say sequence is the sequence of integers beginning as follows:
 1, 11, 21, 1211, 111221, ...
@@ -9,37 +10,8 @@ Given an integer n, generate the nth sequence.
 
 Note: The sequence of integers will be represented as a string.
 """
+__author__ = 'Yang'
 
 class Solution:
     # @return a string
     def countAndSay(self, n):
-        result = '1'
-        if not n:
-            return ''
-        else:
-            while n - 1:
-                result = self.doOnce(result)
-                n -= 1
-            return result
-
-    def doOnce(self, nstr):
-        result = ''
-        if not nstr:
-            return ''
-        else:
-            element = ''
-            times = 0
-            for ele in nstr:
-                if not element:
-                    element = ele
-                    times = 1
-                elif element == ele:
-                    times += 1
-                else:
-                    result += str(times) + element
-                    element = ele
-                    times = 1
-            if element:
-                result += str(times) + element
-            return result
-        
